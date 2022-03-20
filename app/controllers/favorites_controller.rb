eb379c511a79cc9d0c0f833043714c4307c6813d
sakamoto-waka <wakanmhl@gmail.com>
2022-03-20 13:21:37 +0000

    [add] favorites#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,9 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    @book_favorite.save
+    redirect_to book_path(@book_favorite)
+  end  
+  
 end

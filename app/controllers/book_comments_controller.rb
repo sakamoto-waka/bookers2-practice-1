d0326dea47c9092dc1464d50ba22e60f4fe111bf
sakamoto-waka <wakanmhl@gmail.com>
2022-03-20 15:09:09 +0000

    [add] all about destroy

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,5 @@
 class BookCommentsController < ApplicationController
+  
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -6,6 +7,11 @@ class BookCommentsController < ApplicationController
     comment.save
     redirect_back(fallback_location: root_path)
   end
+  
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_back(fallback_location: root_path)
+  end  
 
   private
   def book_comment_params

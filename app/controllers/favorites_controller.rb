bd73adccffe69a23a4974cb62820c0abb1227224
sakamoto-waka <wakanmhl@gmail.com>
2022-03-22 23:19:10 +0000

    [finish] ajax favo

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     favorite.destroy
   end

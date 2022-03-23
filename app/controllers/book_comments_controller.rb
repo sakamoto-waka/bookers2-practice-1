c424d00ef02f6fecd8d7b6466fc7ef52816c9cf6
sakamoto-waka <wakanmhl@gmail.com>
2022-03-23 01:36:03 +0000

    [update] book_comments controller render

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,11 +5,13 @@ class BookCommentsController < ApplicationController
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     @comment.save
+    render :book_comments
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
     @book = Book.find(params[:book_id])
+    render :book_comments
   end
 
   private
